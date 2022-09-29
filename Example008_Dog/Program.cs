b19e1cfe5dde3afd7153f21db85cddac4b97f48a
Michael <mkuchaev@ya.ru>
2022-09-29 13:48:57 +0400

    add Example 008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int distance = 10000;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    }
+
+    if (friend == 2)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    }
+
+    count++;
+    Console.WriteLine(count); 
+}
