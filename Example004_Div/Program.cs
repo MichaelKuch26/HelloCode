a33e2601911d4d9e9819454e068e9e189ab247e1
Michael <mkuchaev@ya.ru>
2022-10-04 17:49:53 +0400

    финальная своя версия

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,15 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+
+
+
+Console.Clear();
+
+double numX = new Random().Next(1, 100);
+Console.WriteLine(numX);
+double numY = new Random().Next(1, 10);
+Console.WriteLine(numY);
+double result = numX / numY;
+Console.Write(result);
