ec02e5ec18cf698f290453cce64b8021f9cffb0b
Michael <mkuchaev@ya.ru>
2022-10-04 17:48:07 +0400

    финальная версия

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите ваше Имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿// Console.Write("Введите вашу кликуху ");
+// string name = Console.ReadLine();
+// Console.Write($"Здорово бродяга по имени {name}");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Скажи свое имя:");
+        string name = Console.ReadLine();
+        Console.Write("Здравие желаю " + name);
+    }
+}
