f9cf65064a78be3d36cfee14067cb67f5582f353
Michael <mkuchaev@ya.ru>
2022-10-04 17:51:27 +0400

    финальная упрощенная версия

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,16 +1,35 @@
-﻿int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+﻿// int a = 1;
+// int b = 20;
+// int c = 6;
+// int d = 8;
+// int e = 4;
 
-int max = a;
+// int max = a;
 
-if(a > max) max = a;
-if(b > max) max = b;
-if(c > max) max = c;
-if(d > max) max = d;
-if(e > max) max = e;
+// if(a > max) max = a;
+// if(b > max) max = b;
+// if(c > max) max = c;
+// if(d > max) max = d;
+// if(e > max) max = e;
 
-Console.Write("max = ");
-Console.WriteLine(max);
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+Console.Clear();
+
+int a = 4;
+int b = 34;
+int c = 32;
+int d = 98;
+int e = 21;
+
+int min = a;
+
+if(a < min) min = a;
+if(b < min) min = b;
+if(c < min) min = c;
+if(d < min) min = d;
+if(e < min) min = e;
+
+Console.Write("min = " + min);
