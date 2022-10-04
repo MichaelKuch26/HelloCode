fca8ca368d5b63ecdca4e23e7c8e9b13615106f8
Michael <mkuchaev@ya.ru>
2022-10-04 17:49:08 +0400

    финальная своя версия

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,14 @@
-﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
+﻿// int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+Console.Clear();
+
+int numX = new Random().Next(1, 100);
+Console.WriteLine(numX);
+int numY = new Random().Next(1, 100);
+Console.WriteLine(numY);
+int result = numX + numY;
 Console.WriteLine(result);
